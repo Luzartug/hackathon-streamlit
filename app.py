f8b811e4445b9c0840559889eec8a058482e124b
Lucas Pascal <lucaspascal@Mac-Lucas.local>
2023-06-13 15:51:05 +0200

    requirement

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from plotly.io import read_json
+import plotly.express as px
 
 st.title('Are you really an activist ?')
 
@@ -34,3 +35,18 @@ st.bar_chart(data=df_choice, x='year', y='engagement')
 st.plotly_chart(read_json("shape1.json"))
 
 st.plotly_chart(read_json("shape3.json"))
+
+
+qf = pd.read_csv("Final.csv")
+tf = qf[['number_of_likes','number_of_shares','sentiment','author']].groupby("author").mean()
+tf["auth"]=tf.index
+fig = px.scatter(tf, x="number_of_likes", size="number_of_shares", y="sentiment", color="auth", hover_name="number_of_shares", title='Climate change influence for each personnality')
+fig.update_layout(xaxis=dict(showgrid=False,
+                             zeroline=False),
+                  yaxis=dict(showgrid=False,
+                             zeroline=False),
+                  hovermode="x",
+                  showlegend=False
+)
+fig.update_traces(hovertemplate=None)
+st.plotly_chart(fig)
